b2fd2d2f5b995db24af47432b49a882e73cefe77
Oprișor Raul-Alexandru <oprisorraul@gmail.com>
2022-10-04 12:33:05 +0300

    Initial Commit

---
+++ src/MyFirst.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Test
+{
+    public static void main(String[] args)
+    {
+        Scanner scann = new Scanner(System.in);
+        int sum = 0;
+        int lastNumber = -1;
+        int counter = -1; // pentru ca acesta creste cu 1 si cand se introduce 0 pentru a opri citirea numerelor
+
+        System.out.println("Input the numbers:");
+        while (lastNumber != 0) {
+            lastNumber = scann.nextInt();
+            sum = sum + lastNumber;
+            counter ++;
+        }
+
+        float average = (float) sum/counter;
+        System.out.println(average);
+    }
+}
